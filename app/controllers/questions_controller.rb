fd5f205750878961e0c53d1ad1931ba6a8977826
Pei Ern Koh <peiern.28@gmail.com>
2021-09-07 12:35:01 +0800

    edited if else statement, added link_to

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -5,5 +5,14 @@ class QuestionsController < ApplicationController
   def answer
     question_input = params[:question]
     @question = question_input
+    @answer = ''
+
+    if @question == 'I am going to work'
+      @answer = 'Great!'
+    elsif @question.include? '?'
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = 'I don\'t care, get dressed and go to work!'
+    end
   end
 end
